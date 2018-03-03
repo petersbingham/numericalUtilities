72d494ab8b9adbb0201d2c54f6c3c939216deb86
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-03 22:03:37 +0000

    Rearrange and rename functions. Add documentation.

--- pynumutil/__init__.py
+++ pynumutil/__init__.py
@@ -1,37 +1,19 @@
-PRECISION = 8
-MIN = pow(10,-PRECISION)
+precision = 8
+min = pow(10,-precision)
 
-def removeduplicateFloats(lst,comparator,accessor=None):
-    def _getVal(lst,i,accessor):
-        if accessor:
-            return accessor(lst[i])
-        else:
-            return lst[i]
-      
-    if len(lst) > 0:
-        iStart = 0
-        while iStart<len(lst)-1:
-            comVal = _getVal(lst,iStart,accessor)
-            for i in range(iStart+1, len(lst)):
-                if comparator.complexCompare(comVal,_getVal(lst,i,accessor)):
-                    lst.pop(i)
-                    break
-                elif i == len(lst)-1:
-                    iStart += 1          
-          
-class Compare:
-    TYPE_ABSOLUTE = 0
-    TYPE_PERCENT = 1
+absolute = 0
+percent = 1
+class nearlyEqual:
     def __init__(self, *args):
         if len(args) == 0:
-            self.realType = self.TYPE_ABSOLUTE
-            self.realVal = MIN
-            self.imagType = self.TYPE_ABSOLUTE
-            self.imagVal = MIN
+            self.realType = absolute
+            self.realVal = min
+            self.imagType = absolute
+            self.imagVal = min
         if len(args) == 1:
-            self.realType = self.TYPE_ABSOLUTE
+            self.realType = absolute
             self.realVal = args[0]
-            self.imagType = self.TYPE_ABSOLUTE
+            self.imagType = absolute
             self.imagVal = args[0]
         elif len(args) == 2:
             self.realType = args[0]
@@ -43,55 +25,55 @@ class Compare:
             self.realVal = args[1]
             self.imagType = args[2]
             self.imagVal = args[3]
-    
+
     def floatCompare(self, f1, f2, complex=False):
         if complex:
             return self._compare(f1, f2, self.imagType, self.imagVal)
         else:
             return self._compare(f1, f2, self.realType, self.realVal)
-      
+
     def complexCompare(self, c1, c2):
         if not self.floatCompare(complex(c1).real, complex(c2).real, False):
             return False
         if not self.floatCompare(complex(c1).imag, complex(c2).imag, True):
             return False
         return True
-    
+
     def _compare(self, f1, f2, type, val):
-        if type == self.TYPE_ABSOLUTE:
+        if type == absolute:
             return self._absCmp(f1, f2, val)
         else:
-            return self._relCmp(f1, f2, val)    
-          
+            return self._relCmp(f1, f2, val)
+
     def _absCmp(self, f1, f2, cmpVal):
         if abs(f1-f2) > cmpVal:
             return False
         return True
-    
+
     def _relCmp(self, f1, f2, cmpVal):
         if abs(f1-f2)/f1*100 > cmpVal:
             return False
         return True
 
 
-class RationalCompare:
-    def __init__(self, zeroValue=0.0, distThreshold=None):
-        self.distThreshold = distThreshold
+class rationalCompare1:
+    def __init__(self, zeroValue=0.0, distThres=None):
+        self.distThres = distThres
         self.zeroValue = zeroValue
 
-    def isClose(self, cval1, cval2, distThreshold=None):
+    def isClose(self, cval1, cval2, distThres=None):
         diff = self.getComplexDiff(cval1, cval2)
-        return self.checkComplexDiff(diff, distThreshold)
+        return self.checkComplexDiff(diff, distThres)
 
     def getComplexDiff(self, cval1, cval2):
         realdiff = self._getDiff(cval2.real, cval1.real)
         imagdiff = self._getDiff(cval2.imag, cval1.imag)
         return realdiff + 1.0j*imagdiff
 
-    def checkComplexDiff(self, cdiff, distThreshold=None):
-        if distThreshold is None:
-            distThreshold = self.distThreshold
-        return cdiff.imag<distThreshold and cdiff.real<distThreshold
+    def checkComplexDiff(self, cdiff, distThres=None):
+        if distThres is None:
+            distThres = self.distThres
+        return cdiff.imag<distThres and cdiff.real<distThres
 
     def _getDiff(self, val1, val2):
         absVal1 = abs(val1)
@@ -105,7 +87,8 @@ class RationalCompare:
         else:
             return self._calDiff(self.zeroValue, val2)
 
-    def _calDiff(self, val1, val2): #https://en.wikipedia.org/wiki/Relative_change_and_difference
+    #https://en.wikipedia.org/wiki/Relative_change_and_difference
+    def _calDiff(self, val1, val2):
         absVal1 = abs(val1)
         absVal2 = abs(val2)
         valMax = absVal2 if (absVal1 < absVal2) else absVal1
@@ -134,7 +117,21 @@ def getPermutations(possVals, numOf):
             for nextPerm in nextPerms:
                 perms.append([val]+nextPerm)
     return perms
-   
-def format_e(n):
-    a = '%E' % n
-    return a.split('E')[0].rstrip('0').rstrip('.') + 'e' + a.split('E')[1]         
+
+def removeduplicateFloats(lst,comparator,accessor=None):
+    def _getVal(lst,i,accessor):
+        if accessor:
+            return accessor(lst[i])
+        else:
+            return lst[i]
+
+    if len(lst) > 0:
+        iStart = 0
+        while iStart<len(lst)-1:
+            comVal = _getVal(lst,iStart,accessor)
+            for i in range(iStart+1, len(lst)):
+                if comparator.complexCompare(comVal,_getVal(lst,i,accessor)):
+                    lst.pop(i)
+                    break
+                elif i == len(lst)-1:
+                    iStart += 1
