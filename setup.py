f91c52d41738f1918d1a80177c9f67cd95c2a0b3
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:06:07 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'pynumutil/README.md')
 
 setup(name='pynumutil',
-      version='0.1',
+      version='0.2',
       description='Simple Numerical Utilities',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['pynumutil'],
+      package_data={'pynumutil': ['README.md'],
      )
